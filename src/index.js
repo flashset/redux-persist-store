7010a963685f097ee68614c7709d1088d4679a1b
spatrame <shashidhara.p@tetherfi.com>
2021-06-08 20:45:38 +0530

    wrapper name changed as component

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-export { default as persistReducer } from "./persistReducer";
+export { default as PersistWrapper } from "./persistWrapper";
 export { loadState as persistState } from "./persistManager";
